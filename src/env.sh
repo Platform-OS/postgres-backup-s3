58b01a74b453727fdfdeb069efbf0ed4597fed29
dariusz gorzeba <dgorzeba@gmail.com>
2025-02-10 22:02:51 +0100

    allow to use either DATABASE_URL or POSTGRES_HOST

--- src/env.sh
+++ src/env.sh
@@ -1,3 +1,5 @@
+set -e
+
 if [ -z "$S3_BUCKET" ]; then
   echo "You need to set the S3_BUCKET environment variable."
   exit 1
@@ -8,8 +10,9 @@ if [ -z "$POSTGRES_DATABASE" ]; then
   exit 1
 fi
 if [ -z "$DATABASE_URL" ]; then
-  echo "You need to set the DATABASE_URL environment variable."
-  exit 1
+  echo "DATABASE_URL not set. Building from POSTGRES_[USER|PASSWORD|HOST|DATABASE]"
+  set -u
+  export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}"
 fi
 
 export AWS_DEFAULT_REGION=$S3_REGION
